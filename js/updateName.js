c91810a5dc4171bc15570056cf9c16003c77bd89
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-09-05 07:26:02 +0100

    Pass obj{} into updateName()

--- js/updateName.js
+++ js/updateName.js
@@ -2,11 +2,11 @@ const updateName = () => {
 	const input = document.querySelector('.js-input-name');
 	const text1 = document.querySelector('.js-text-1');
 
-	const updateText = (text) => text1.textContent = text;
+	const updateText = (options) => text1.textContent = options.text;
 
 	const textObserver = new Observable();
 	textObserver.subscribe(updateText);
 
-	input.addEventListener('keyup', e => textObserver.notify(e.target.value));
+	input.addEventListener('keyup', e => textObserver.notify({text: e.target.value}));
 };
 updateName();
