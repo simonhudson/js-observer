a76bbdc1ec3028b0ae2afeabc696f28b7a1f5443
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-08-20 13:15:11 +0100

    update UI

--- js/dynamicForm.js
+++ js/dynamicForm.js
@@ -1,13 +1,12 @@
 const hiddenClass = 'visuallyhidden';
 
-
 const dynamicForm = () => {
 
 	const setElementVisibility = (options) => {
 		const elements = options.elements;
 		const value = options.value;
 		elements.forEach(element => {
-			let showIfValues = element.dataset.showIfValue.split(',').map(item => item.trim());
+			const showIfValues = element.dataset.showIfValue.split(',').map(item => item.trim());
 			const method = (showIfValues.indexOf(value) > -1) ? 'remove' : 'add';
 			element.classList[method](hiddenClass);
 		});
@@ -24,8 +23,8 @@ const dynamicForm = () => {
 		elements.forEach(element => {
 			const elementObserver = new Observable();
 			elementObserver.subscribe(setElementVisibility);
-			const name = element.getAttribute('name');
-			const dependentElements = Array.from(document.querySelectorAll(`[data-show-if-field="${name}"]`));
+			const id = element.getAttribute('id');
+			const dependentElements = Array.from(document.querySelectorAll(`[data-show-if-field="${id}"]`));
 			const events = ['keyup', 'change'];
 			events.forEach(evt => {
 				element.addEventListener(evt, e => {
